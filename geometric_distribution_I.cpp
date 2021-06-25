31e433436e12548f649cc730c4110f829de663dc
Navneet Kumar Chaurasiya <32726683+nkwarrior84@users.noreply.github.com>
2021-06-25 12:10:33 +0530

    geometic 2

--- geometric_distribution_I.cpp
+++ geometric_distribution_I.cpp
@@ -24,3 +24,36 @@ int main() {
     printf("%0.3f", g_prob);
     return 0;
 }
+
+/*
+The probability that a machine produces a defective product is 1/3. What is the probability that the 1st defect occurs in first 5 item produced?
+*/
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    
+    double p = 0.333;
+    double q = 1 - p;
+    
+    double g_prob = 0.0;
+    
+    for(auto i=1; i<=5; i++)
+    {
+        g_prob += pow(q, i-1)*p;
+    }
+    
+    printf("%0.3f", g_prob);
+    
+    return 0;
+}
+
+
